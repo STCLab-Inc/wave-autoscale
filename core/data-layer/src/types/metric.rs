cf0e592d0767e62c7ffe4173e5fbba1fb8ae8e27
Hwansoo Kim <pueding@gmail.com>
2023-03-27 23:33:42 +0900

    refactor(metric.rs): change metadata type from HashMap<String, String> to HashMap<String, Value>

--- core/data-layer/src/types/metric.rs
+++ core/data-layer/src/types/metric.rs
@@ -1,11 +1,13 @@
 use super::object_kind::ObjectKind;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::collections::HashMap;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
+
 pub struct Metric {
     pub kind: ObjectKind,
     pub id: String,
     pub metric_kind: String,
-    pub metadata: HashMap<String, String>,
+    pub metadata: HashMap<String, Value>,
 }
