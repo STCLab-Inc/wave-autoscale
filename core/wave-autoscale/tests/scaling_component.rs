8e71c0f4518861dfa4b66d8435fa71a0258ba591
Hwansoo Kim <pueding@gmail.com>
2023-04-12 15:19:32 +0900

    feat(scaling_component): k8s deployment

--- core/wave-autoscale/tests/scaling_component.rs
+++ core/wave-autoscale/tests/scaling_component.rs
@@ -6,7 +6,8 @@ mod scaling_component_test {
     use data_layer::reader::yaml_reader::read_yaml_file;
     use serde_json::{json, Value};
     use wave_autoscale::scaling_component::{
-        aws_ec2_autoscaling::EC2AutoScalingComponent, ScalingComponentManagerInner,
+        aws_ec2_autoscaling::EC2AutoScalingComponent,
+        k8s_deployment::K8sDeploymentScalingComponent, ScalingComponentManagerInner,
     };
 
     const EC2_AUTOSCALING_FILE_PATH: &str = "./tests/yaml/component_ec2_autoscaling.yaml";
@@ -40,5 +41,33 @@ mod scaling_component_test {
             .await;
         return result;
         Ok(())
+    }
+    #[tokio::test]
+    async fn k8s_deployment_autoscaling() -> Result<()> {
+        // read yaml file
+        let result = read_yaml_file("./tests/yaml/component_k8s_deployment.yaml")?;
+        // create metric adapter
+        let mut scaling_component_manager = ScalingComponentManagerInner::new();
+        scaling_component_manager.add_definitions(result.scaling_component_definitions);
+
+        if let Some(scaling_component) =
+            scaling_component_manager.get_scaling_component("k8s_deployment")
+        {
+            let name = scaling_component.get_scaling_component_kind();
+            assert!(
+                name == K8sDeploymentScalingComponent::TRIGGER_KIND,
+                "Unexpected"
+            );
+        } else {
+            assert!(false, "No scaling component found")
+        }
+
+        // run scaling trigger
+        let mut options: HashMap<String, Value> = HashMap::new();
+        options.insert("replicas".to_string(), json!(5));
+        let result = scaling_component_manager
+            .apply_to("k8s_deployment", options)
+            .await;
+        return result;
     }
 }
