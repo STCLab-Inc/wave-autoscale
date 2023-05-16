518577cab4841e138394a4bcedb550f2fb54da57
Hwansoo Kim <pueding@gmail.com>
2023-05-16 23:54:07 +0900

    feat(web-app): add support for scaling plan metadata for EC2 Autoscaling and k8s Deployment components

--- core/web-app/src/utils/scaling-component-binding.ts
+++ core/web-app/src/utils/scaling-component-binding.ts
@@ -1,7 +1,9 @@
 import { getInterfaceKeyTypes, MetadataKeyType } from './metadata-key-type';
 import { EC2AutoscalingMetadata } from '@/types/bindings/ec2-autoscaling';
+import { EC2AutoscalingPlanMetadata } from '@/types/bindings/ec2-autoscaling-plan';
 import { K8sDeploymentMetadata } from '@/types/bindings/k8s-deployment';
 import { ScalingComponentDefinition } from '@/types/bindings/scaling-component-definition';
+import { K8sDeploymentPlanMetadata } from '../types/bindings/k8s-deployment-plan';
 
 export type ScalingComponentKeyType = {
   componentName: string;
@@ -28,6 +30,34 @@ export function getScalingComponentKeyTypes(): ScalingComponentKeyType[] {
     api_server_endpoint: '',
     namespace: '',
     name: '',
+  });
+  componentKeyTypes.push({
+    componentName: 'kubernetes-deployment',
+    keyTypes: k8sDeploymentComponent,
+  });
+
+  return componentKeyTypes;
+}
+
+// For ScalingPlan
+export function getScalingComponentPlanKeyTypes(): ScalingComponentKeyType[] {
+  const componentKeyTypes: ScalingComponentKeyType[] = [];
+
+  // EC2 Autoscaling
+  const ec2autoscalingComponentPlan =
+    getInterfaceKeyTypes<EC2AutoscalingPlanMetadata>({
+      desired: 0,
+      min: 0,
+      max: 0,
+    });
+  componentKeyTypes.push({
+    componentName: 'aws-ec2-autoscaling',
+    keyTypes: ec2autoscalingComponentPlan,
+  });
+
+  // k8s Deployment
+  const k8sDeploymentComponent = getInterfaceKeyTypes<K8sDeploymentPlanMetadata>({
+    replicas: 0,
   });
   componentKeyTypes.push({
     componentName: 'kubernetes-deployment',
