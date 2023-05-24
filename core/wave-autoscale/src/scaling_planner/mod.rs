0795180ac22abcca26fa589c8ed352a173bf93cb
Hwansoo Kim <pueding@gmail.com>
2023-05-24 11:07:07 +0900

    feat(controller): add autoscaling history to the scaling planner

--- core/wave-autoscale/src/scaling_planner/mod.rs
+++ core/wave-autoscale/src/scaling_planner/mod.rs
@@ -1,7 +1,14 @@
 use crate::{metric_store::MetricStore, scaling_component::ScalingComponentManager};
+use ::data_layer::data_layer::DataLayer;
 use anyhow::Result;
-use data_layer::{types::plan_item_definition::PlanItemDefinition, ScalingPlanDefinition};
-use serde_json::Value;
+use data_layer::{
+    types::{
+        autoscaling_history_definition::AutoscalingHistoryDefinition,
+        plan_item_definition::PlanItemDefinition,
+    },
+    ScalingPlanDefinition,
+};
+use serde_json::{json, Value};
 use std::{collections::HashMap, sync::Arc, time::Duration};
 use tokio::{sync::RwLock, time};
 
@@ -58,7 +65,8 @@ fn get_matching_scaling_plan<'a>(
 async fn apply_scaling_components(
     scaling_components_metadata: &[Value],
     shared_scaling_component_manager: &ScalingComponentManager,
-) {
+) -> Vec<Result<()>> {
+    let mut scaling_results: Vec<Result<()>> = Vec::new();
     for metadata in scaling_components_metadata.iter() {
         let scaling_component_id = metadata["component_id"].as_str().unwrap();
         let shared_scaling_component_manager = shared_scaling_component_manager.read().await;
@@ -70,10 +78,12 @@ async fn apply_scaling_components(
             .map(|(key, value)| (key.to_string(), value.clone()))
             .collect::<HashMap<String, Value>>();
 
-        shared_scaling_component_manager
+        let result = shared_scaling_component_manager
             .apply_to(scaling_component_id, params)
             .await;
+        scaling_results.push(result);
     }
+    scaling_results
 }
 
 pub struct ScalingPlanner {
@@ -81,6 +91,7 @@ pub struct ScalingPlanner {
     metric_store: MetricStore,
     scaling_component_manager: ScalingComponentManager,
     last_plan_id: Arc<RwLock<String>>,
+    data_layer: Arc<DataLayer>,
 }
 
 impl<'a> ScalingPlanner {
@@ -88,12 +99,14 @@ impl<'a> ScalingPlanner {
         definition: ScalingPlanDefinition,
         metric_store: MetricStore,
         scaling_component_manager: ScalingComponentManager,
+        data_layer: Arc<DataLayer>,
     ) -> Self {
         ScalingPlanner {
             definition,
             metric_store,
             scaling_component_manager,
             last_plan_id: Arc::new(RwLock::new(String::new())),
+            data_layer,
         }
     }
     fn sort_plan_by_priority(&self) -> Vec<PlanItemDefinition> {
@@ -109,6 +122,8 @@ impl<'a> ScalingPlanner {
         let shared_metric_store = self.metric_store.clone();
         let shared_scaling_component_manager = self.scaling_component_manager.clone();
         let shared_last_run = self.last_plan_id.clone();
+        let scaling_plan_definition = self.definition.clone();
+        let data_layer = self.data_layer.clone();
 
         tokio::spawn(async move {
             loop {
@@ -127,16 +142,38 @@ impl<'a> ScalingPlanner {
 
                         let scaling_components_metadata = &plan.scaling_components;
                         // Apply the scaling components
-                        apply_scaling_components(
+                        let results = apply_scaling_components(
                             scaling_components_metadata,
                             &shared_scaling_component_manager,
                         )
                         .await;
-
+                        println!("results - {:?}", results);
                         // Update the last run
                         let mut shared_last_run = shared_last_run.write().await;
                         *shared_last_run = scaling_plan_id.clone();
                         println!("Applied scaling plan: {}", scaling_plan_id);
+
+                        // Add the result of the scaling plan to the history
+                        for (index, result) in results.iter().enumerate() {
+                            let fail_message: Option<String> = match result {
+                                Ok(_) => None,
+                                Err(error) => Some(error.to_string()),
+                            };
+                            let autoscaling_history: AutoscalingHistoryDefinition =
+                                AutoscalingHistoryDefinition::new(
+                                    scaling_plan_definition.db_id.clone(),
+                                    scaling_plan_definition.id.clone(),
+                                    json!(plan).to_string(),
+                                    json!(shared_metric_store.clone()).to_string(),
+                                    json!(scaling_components_metadata[index].clone()).to_string(),
+                                    fail_message,
+                                );
+                            println!("autoscaling_history - {:?}", autoscaling_history);
+                            let result = data_layer
+                                .add_autoscaling_history(autoscaling_history)
+                                .await;
+                            println!("result - {:?}", result);
+                        }
                     } else {
                         println!("Already executed");
                     }
