0257a1def65124a68492ea4ea3fa86c3110f228b
Hwansoo Kim <pueding@gmail.com>
2023-06-15 18:38:16 +0900

    feat(cli): add new arguments to Args struct: config, plan, watch_plan, except_api_server, run_web_app

--- core/wave-cli/src/args.rs
+++ core/wave-cli/src/args.rs
@@ -8,12 +8,23 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    #[arg(short, long)]
-    pub plan: Option<String>,
+    /// Read configuration from a yaml file. If not specified, the default config path './wave-config.yaml' will be used.
     #[arg(short, long)]
     pub config: Option<String>,
+
+    /// Read initial plans from a yaml file. This plans will be saved into the database. If not specified, the default plans path './plan.yaml' will be used.
     #[arg(short, long)]
+    pub plan: Option<String>,
+
+    /// Watch the plan file for changes and reload accodingly
+    #[arg(long)]
+    pub watch_plan: bool,
+
+    /// Do not run Wave API Server
+    #[arg(long)]
     pub except_api_server: bool,
-    #[arg(short, long)]
+
+    /// Run Wave Web App(Web Interface)
+    #[arg(long)]
     pub run_web_app: bool,
 }
