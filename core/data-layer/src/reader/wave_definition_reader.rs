c0fcebf35b32c298f597a37427ee0ace6257c679
Bryan921105 <bryan@stclab.com>
2023-07-31 22:43:24 +0900

    feat(data-layer): add features for variable sources

--- core/data-layer/src/reader/wave_definition_reader.rs
+++ core/data-layer/src/reader/wave_definition_reader.rs
@@ -2,7 +2,7 @@ use crate::{MetricDefinition, ScalingComponentDefinition, ScalingPlanDefinition,
 use anyhow::Result;
 use dotenv_parser::parse_dotenv;
 use handlebars::Handlebars;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use serde_valid::Validate;
 use serde_yaml::Deserializer;
 use std::{
@@ -20,36 +20,34 @@ pub struct ParserResult {
     pub scaling_component_definitions: Vec<ScalingComponentDefinition>,
 }
 
-// Function to remove backslash quotes from a json string
-fn remove_backslash_quotes(data: &str) -> String {
-    data.replace("\"\\", "")
-        .replace("\\\"", "")
-        .replace('\"', "")
-}
-/* TODO! */
-// Function to get the output paths
-fn get_variables_paths<P: AsRef<Path>>(input: P) -> (String, String, String) {
+// Function to get the source paths
+fn get_source_paths<P: AsRef<Path>>(input: P) -> (String, String, String) {
     // Convert the input path to a path
     let path = input.as_ref();
 
     // Get the parent directory of the input path
     let parent_path = path.parent().unwrap_or(Path::new(""));
 
-    // Construct the new file paths using PathBuf
-    let output_yaml = parent_path.join("variables.yaml");
-    let output_env = parent_path.join("variables.env");
-    let output_json = parent_path.join("variables.json");
+    // Construct the new file paths using path bufs
+    let yaml_source_path = parent_path.join("variables.yaml");
+    let env_source_path = parent_path.join("variables.env");
+    let json_source_path = parent_path.join("variables.json");
 
-    // Convert the PathBufs to strings
-    let output_yaml_str = output_yaml.to_str().unwrap().to_string();
-    let output_env_str = output_env.to_str().unwrap().to_string();
-    let output_json_str = output_json.to_str().unwrap().to_string();
+    // Convert the path bufs to strings
+    let yaml_source_path_str = yaml_source_path.to_str().unwrap().to_string();
+    let env_source_path_str = env_source_path.to_str().unwrap().to_string();
+    let json_source_path_str = json_source_path.to_str().unwrap().to_string();
 
-    // Return the output file paths
-    (output_yaml_str, output_env_str, output_json_str)
+    // Return the source paths
+    (
+        yaml_source_path_str,
+        env_source_path_str,
+        json_source_path_str,
+    )
 }
 
-fn parse_variable_from_yaml_file(file_path: &str) -> Option<serde_json::Value> {
+// Function to extract parameters from a yaml file
+fn extract_params_from_yaml_file(file_path: &str) -> Option<serde_json::Value> {
     let file_string = fs::read_to_string(file_path).ok()?;
     let file_yaml: serde_yaml::Value = serde_yaml::from_str(&file_string).ok()?;
     if file_yaml.as_mapping().is_none() {
@@ -59,29 +57,28 @@ fn parse_variable_from_yaml_file(file_path: &str) -> Option<serde_json::Value> {
         let mut file_hashmap = HashMap::new();
         for (key, value) in file_map.iter() {
             if let (Some(key_str), Some(value_str)) = (key.as_str(), value.as_str()) {
-                file_hashmap.insert(
-                    remove_backslash_quotes(key_str),
-                    remove_backslash_quotes(value_str),
-                );
+                file_hashmap.insert(key_str, value_str);
             }
         }
-        Some(serde_json::Value::String(
-            serde_json::to_string(&file_hashmap).unwrap(),
-        ))
+        Some(serde_json::to_value(file_hashmap).unwrap())
     }
 }
-fn parse_variable_from_env_file(file_path: &str) -> Option<serde_json::Value> {
+// Function to extract parameters from an env file
+fn extract_params_from_env_file(file_path: &str) -> Option<serde_json::Value> {
     let file_string = fs::read_to_string(file_path).ok()?;
     let file_map = parse_dotenv(&file_string).unwrap();
     let mut file_hashmap = HashMap::new();
     for (key, value) in file_map.iter() {
-        file_hashmap.insert(remove_backslash_quotes(key), remove_backslash_quotes(value));
+        file_hashmap.insert(key, value);
     }
-    Some(serde_json::Value::String(
-        serde_json::to_string(&file_hashmap).unwrap(),
-    ))
+    Some(serde_json::to_value(file_hashmap).unwrap())
 }
-fn parse_variable_from_json_file(file_path: &str) -> Option<serde_json::Value> {
+// Function to remove backslash quotes from a json string
+fn remove_backslash_quotes(data: &str) -> String {
+    data.replace('\"', "")
+}
+// Function to extract parameters from a json file
+fn extract_params_from_json_file(file_path: &str) -> Option<serde_json::Value> {
     let file_string = fs::read_to_string(file_path).ok()?;
     let file_json: serde_json::Value = serde_json::from_str(&file_string).ok()?;
     if file_json.as_object().is_none() {
@@ -95,46 +92,24 @@ fn parse_variable_from_json_file(file_path: &str) -> Option<serde_json::Value> {
                 remove_backslash_quotes(&value.to_string()),
             );
         }
-        Some(serde_json::Value::String(
-            serde_json::to_string(&file_hashmap).unwrap(),
-        ))
+        Some(serde_json::to_value(file_hashmap).unwrap())
     }
 }
-fn get_params<P>(path: P) -> serde_json::Value
+// Function to get parameters for handlebars
+fn get_params_for_handlebars<P>(path: P) -> serde_json::Value
 where
     P: AsRef<Path>,
 {
-    let mut params_for_handlebars: serde_json::Value = serde_json::json!({});
     let (variables_path_from_yaml, variables_path_from_env, variables_path_from_json) =
-        get_variables_paths(&path);
-    println!("variables_path_from_yaml: {:?}", &variables_path_from_yaml);
-    println!("variables_path_from_env: {:?}", &variables_path_from_env);
-    println!("variables_path_from_json: {:?}", &variables_path_from_json);
-
-    if let Some(variables_from_yaml) = parse_variable_from_yaml_file(&variables_path_from_yaml) {
-        if let serde_json::Value::Object(map) = &mut params_for_handlebars {
-            println!("variables_from_yaml: {:?}", variables_from_yaml);
-            map.insert("yaml".to_string(), variables_from_yaml);
-        }
-        println!("params_for_handlebars: {:?}", params_for_handlebars);
-    }
-
-    if let Some(variables_from_env) = parse_variable_from_env_file(&variables_path_from_env) {
-        if let serde_json::Value::Object(map) = &mut params_for_handlebars {
-            println!("variables_from_env: {:?}", variables_from_env);
-            map.insert("env".to_string(), variables_from_env);
-        }
-        println!("params_for_handlebars: {:?}", params_for_handlebars);
-    }
-
-    if let Some(variables_from_json) = parse_variable_from_json_file(&variables_path_from_json) {
-        if let serde_json::Value::Object(map) = &mut params_for_handlebars {
-            println!("variables_from_json: {:?}", variables_from_json);
-            map.insert("json".to_string(), variables_from_json);
-        }
-        println!("params_for_handlebars: {:?}", params_for_handlebars);
-    }
-    params_for_handlebars
+        get_source_paths(&path);
+    let variables_from_yaml = extract_params_from_yaml_file(&variables_path_from_yaml);
+    let variables_from_env = extract_params_from_env_file(&variables_path_from_env);
+    let variables_from_json = extract_params_from_json_file(&variables_path_from_json);
+    serde_json::json!({
+        "yaml": serde_json::json!(variables_from_yaml),
+        "env": serde_json::json!(variables_from_env),
+        "json": serde_json::json!(variables_from_json),
+    })
 }
 
 pub fn read_definition_yaml_file<P>(path: P) -> Result<ParserResult>
@@ -143,21 +118,16 @@ where
 {
     // Read the file of the path
     let mut file = File::open(&path)?;
-    /*  */
     // Get parameters for handlebars
-    let params_for_handlebars = get_params(&path);
-    println!("{:?}", params_for_handlebars);
-    /*  */
+    let params_for_handlebars = get_params_for_handlebars(&path);
     // Read to a string from the file
     let mut file_string = String::new();
     file.read_to_string(&mut file_string)?;
-    // Render the file with the variable json using handlebars
+    // Render the file with the variables using handlebars
     let handlebars = Handlebars::new();
-    println!("params_for_handlebars: {:?}", params_for_handlebars);
     let new_file = handlebars.render_template(file_string.as_str(), &params_for_handlebars)?;
+    // Make a cursor to read the new file
     let file_cursor = std::io::Cursor::new(new_file.as_bytes());
-    println!("new_file: {:?}", new_file);
-    /*  */
     // Make a deserializer to iterate the yaml that could have multiple documents
     let deserializer = Deserializer::from_reader(file_cursor);
     // For result
