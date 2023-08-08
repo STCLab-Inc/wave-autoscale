004d4bb3ab0e1747da35b64d6855184f025a6f02
Bryan921105 <bryan@stclab.com>
2023-08-08 22:00:55 +0900

    feat(controller): integrate similar functions in google cloud run service helper

--- core/wave-autoscale/src/util/google_cloud/google_cloud_run_service_helper.rs
+++ core/wave-autoscale/src/util/google_cloud/google_cloud_run_service_helper.rs
@@ -4,16 +4,22 @@ use reqwest::{Client, Error, Response};
 use serde::{Deserialize, Serialize};
 
 // v1 and v2    - https://cloud.google.com/run/docs/reference/about-apis
-
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct CloudRunFirstVersionGetServiceSetting {
-    pub location_name: String,
+pub struct CloudRunGetServiceSetting {
+    pub api_version: String,
     pub project_name: String,
+    pub location_name: String,
     pub service_name: String,
 }
+pub async fn call_get_cloud_run_service(cloud_run_service_setting: CloudRunGetServiceSetting)-> Result<Response, Error>{
+match cloud_run_service_setting.api_version.as_str() {
+    "v1" => call_get_first_version_cloud_run_service(cloud_run_service_setting).await,
+    _ =>  call_get_second_version_cloud_run_service(cloud_run_service_setting).await,    
+}
+}
 // v1   - https://cloud.google.com/run/docs/reference/rest/v1/namespaces.services/get
-pub async fn call_get_cloud_run_first_version_service(
-    cloud_run_service_setting: CloudRunFirstVersionGetServiceSetting,
+pub async fn call_get_first_version_cloud_run_service(
+    cloud_run_service_setting: CloudRunGetServiceSetting,
 ) -> Result<Response, Error> {
     Client::new()
         .get(format!(
@@ -26,39 +32,9 @@ pub async fn call_get_cloud_run_first_version_service(
         .send()
         .await
 }
-
-pub struct CloudRunFirstVersionPutServiceSetting {
-    pub location_name: String,
-    pub project_name: String,
-    pub service_name: String,
-    pub payload: Option<serde_json::Value>,
-}
-// v1   - https://cloud.google.com/run/docs/reference/rest/v1/namespaces.services/replaceService
-pub async fn call_put_cloud_run_first_version_service(
-    cloud_run_service_setting: CloudRunFirstVersionPutServiceSetting,
-) -> Result<Response, Error> {
-    Client::new()
-        .put(format!(
-            "https://{location_name}-run.googleapis.com/apis/serving.knative.dev/v1/namespaces/{project_name}/services/{service_name}",            
-            location_name = &cloud_run_service_setting.location_name,
-            project_name = &cloud_run_service_setting.project_name,
-            service_name = &cloud_run_service_setting.service_name,            
-        ))
-        .bearer_auth(get_gcp_credential_token().await.unwrap_or("".to_string()))
-        .json(&cloud_run_service_setting.payload)
-        .send()
-        .await
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct CloudRunSecondVersionGetServiceSetting {
-    pub project_name: String,
-    pub location_name: String,
-    pub service_name: String,
-}
 // v2   - https://cloud.google.com/run/docs/reference/rest/v2/projects.locations.services/get
-pub async fn call_get_cloud_run_second_version_service(
-    cloud_run_service_setting: CloudRunSecondVersionGetServiceSetting,
+pub async fn call_get_second_version_cloud_run_service(
+    cloud_run_service_setting: CloudRunGetServiceSetting,
 ) -> Result<Response, Error> {
     Client::new()
         .get(format!(
@@ -72,15 +48,40 @@ pub async fn call_get_cloud_run_second_version_service(
         .await
 }
 
-pub struct CloudRunSecondVersionPatchServiceSetting {
+// v1 and v2    - https://cloud.google.com/run/docs/reference/about-apis
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct CloudRunPatchServiceSetting {
+    pub api_version: String,
     pub project_name: String,
-    pub location_name: String,    
+    pub location_name: String,
     pub service_name: String,
     pub payload: Option<serde_json::Value>,
 }
+pub async fn call_patch_cloud_run_service(cloud_run_service_setting: CloudRunPatchServiceSetting)-> Result<Response, Error>{
+match cloud_run_service_setting.api_version.as_str() {
+    "v1" => call_put_first_version_cloud_run_service(cloud_run_service_setting).await,
+    _ =>  call_patch_second_version_cloud_run_service(cloud_run_service_setting).await,    
+}
+}
+// v1   - https://cloud.google.com/run/docs/reference/rest/v1/namespaces.services/replaceService
+pub async fn call_put_first_version_cloud_run_service(
+    cloud_run_service_setting: CloudRunPatchServiceSetting,
+) -> Result<Response, Error> {
+    Client::new()
+        .put(format!(
+            "https://{location_name}-run.googleapis.com/apis/serving.knative.dev/v1/namespaces/{project_name}/services/{service_name}",            
+            location_name = &cloud_run_service_setting.location_name,
+            project_name = &cloud_run_service_setting.project_name,
+            service_name = &cloud_run_service_setting.service_name,            
+        ))
+        .bearer_auth(get_gcp_credential_token().await.unwrap_or("".to_string()))
+        .json(&cloud_run_service_setting.payload)
+        .send()
+        .await
+}
 // v2   - https://cloud.google.com/run/docs/reference/rest/v2/projects.locations.services/patch
-pub async fn call_patch_cloud_run_second_version_service(
-    cloud_run_service_setting: CloudRunSecondVersionPatchServiceSetting,
+pub async fn call_patch_second_version_cloud_run_service(
+    cloud_run_service_setting: CloudRunPatchServiceSetting,
 ) -> Result<Response, Error> {
     Client::new()
         .patch(format!(
@@ -101,21 +102,22 @@ mod test {
 
     #[ignore]
     #[tokio::test]
-    async fn test_call_get_cloud_run_first_version_service() {
-        let cloud_run_service_setting = CloudRunFirstVersionGetServiceSetting {            
+    async fn test_call_get_first_version_cloud_run_service() {
+        let cloud_run_service_setting = CloudRunGetServiceSetting {   
+            api_version: "v1".to_string(),         
             location_name: "asia-northeast2".to_string(),
             project_name: "wave-autoscale-test".to_string(),               
             service_name: "service-1".to_string(),                           
         };
 
-        let response = call_get_cloud_run_first_version_service(cloud_run_service_setting)
+        let response = call_get_cloud_run_service(cloud_run_service_setting)
             .await
             .unwrap();
 
         let status = response.status();
         let body = response.text().await.unwrap_or("".to_string());
         println!(
-            "test_call_get_cloud_run_first_version_service contents: {:?}",
+            "test_call_get_first_version_cloud_run_service contents: {:?}",
             body
         );
 
@@ -124,8 +126,9 @@ mod test {
 
     #[ignore]
     #[tokio::test]
-    async fn test_call_put_cloud_run_first_version_service() {
-        let cloud_run_service_setting = CloudRunFirstVersionPutServiceSetting {
+    async fn test_call_put_first_version_cloud_run_service() {
+        let cloud_run_service_setting = CloudRunPatchServiceSetting {
+            api_version: "v1".to_string(),
             location_name: "asia-northeast2".to_string(),
             project_name: "wave-autoscale-test".to_string(),            
             service_name: "service-1".to_string(),
@@ -158,14 +161,14 @@ mod test {
             })),            
         };
 
-        let response = call_put_cloud_run_first_version_service(cloud_run_service_setting)
+        let response = call_patch_cloud_run_service(cloud_run_service_setting)
             .await
             .unwrap();
 
         let status = response.status();
         let body = response.text().await.unwrap_or("".to_string());
         println!(
-            "test_call_put_cloud_run_first_version_service contents: {:?}",
+            "test_call_put_first_version_cloud_run_service contents: {:?}",
             body
         );
 
@@ -174,21 +177,22 @@ mod test {
     
     #[ignore]
     #[tokio::test]
-    async fn test_call_get_cloud_run_second_version_service() {
-        let cloud_run_service_setting = CloudRunSecondVersionGetServiceSetting {            
+    async fn test_call_get_second_version_cloud_run_service() {
+        let cloud_run_service_setting = CloudRunGetServiceSetting {     
+            api_version: "v2".to_string(),                
             project_name: "wave-autoscale-test".to_string(),               
             location_name: "asia-northeast2".to_string(),
             service_name: "service-1".to_string(),                           
         };
 
-        let response = call_get_cloud_run_second_version_service(cloud_run_service_setting)
+        let response = call_get_cloud_run_service(cloud_run_service_setting)
             .await
             .unwrap();
 
         let status = response.status();
         let body = response.text().await.unwrap_or("".to_string());
         println!(
-            "test_call_get_cloud_run_second_version_service contents: {:?}",
+            "test_call_get_second_version_cloud_run_service contents: {:?}",
             body
         );
 
@@ -197,8 +201,9 @@ mod test {
 
     #[ignore]
     #[tokio::test]
-    async fn test_call_patch_cloud_run_second_version_service() {
-        let cloud_run_service_setting = CloudRunSecondVersionPatchServiceSetting {
+    async fn test_call_patch_second_version_cloud_run_service() {
+        let cloud_run_service_setting = CloudRunPatchServiceSetting {
+            api_version: "v2".to_string(),
             project_name: "wave-autoscale-test".to_string(),
             location_name: "asia-northeast2".to_string(),
             service_name: "service-1".to_string(),
@@ -219,17 +224,17 @@ mod test {
             })),            
         };
 
-        let response = call_patch_cloud_run_second_version_service(cloud_run_service_setting)
+        let response = call_patch_cloud_run_service(cloud_run_service_setting)
             .await
             .unwrap();
 
         let status = response.status();
         let body = response.text().await.unwrap_or("".to_string());
         println!(
-            "test_call_patch_cloud_run_second_version_service contents: {:?}",
+            "test_call_patch_second_version_cloud_run_service contents: {:?}",
             body
         );
-
+        
         assert!(status == reqwest::StatusCode::OK);
     }
 
