5857d5cbec7e0925888ce098075cbb448978849f
Bryan921105 <bryan@stclab.com>
2023-11-14 01:19:13 +0900

    fix(web-app): edit web-app content header

--- core/web-app/src/app/app/content-header.tsx
+++ core/web-app/src/app/app/content-header.tsx
@@ -13,17 +13,13 @@ export default function ContentHeader({
   return (
     <div
       className={classNames(
-        'h-22 prose flex w-full min-w-full flex-col border-b border-base-300 bg-slate-50 px-3 pt-3',
-        {
-          'pb-3': !children,
-        }
+        'flex h-14 w-full min-w-full flex-row items-center justify-between border-b border-t border-gray-200 bg-gray-75 '
       )}
     >
-      {/* Plan Title */}
-      <div className="flex-start flex">
-        <h3 className="m-3 flex-1">{title}</h3>
-        {right}
+      <div className="font-Pretendard whitespace-nowrap pl-8 pr-4 text-lg font-semibold text-gray-1000">
+        {title}
       </div>
+      <div className="flex pl-4 pr-8">{right}</div>
       {children}
     </div>
   );
