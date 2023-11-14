7154852611fa35803649f8157959f3cf5d344f10
Bryan921105 <bryan@stclab.com>
2023-11-15 00:38:48 +0900

    refactor(web-app): content header using interface

--- core/web-app/src/app/app/content-header.tsx
+++ core/web-app/src/app/app/content-header.tsx
@@ -1,21 +1,18 @@
-import classNames from 'classnames';
 import { ReactNode } from 'react';
+
+interface ContentHeaderProps {
+  title: string;
+  children?: ReactNode;
+  right?: ReactNode;
+}
 
 export default function ContentHeader({
   title,
   children,
   right,
-}: {
-  title: string;
-  children?: ReactNode;
-  right?: ReactNode;
-}) {
+}: ContentHeaderProps) {
   return (
-    <div
-      className={classNames(
-        'flex h-14 w-full min-w-full flex-row items-center justify-between border-b border-t border-gray-200 bg-gray-75 '
-      )}
-    >
+    <div className="flex h-14 w-full min-w-full flex-row items-center justify-between border-b border-t border-gray-200 bg-gray-75">
       <div className="font-Pretendard whitespace-nowrap pl-8 pr-4 text-lg font-semibold text-gray-1000">
         {title}
       </div>
