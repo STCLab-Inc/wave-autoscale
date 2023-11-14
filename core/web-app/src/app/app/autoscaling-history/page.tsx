6bfef31764cefafe5062565dd87e4c42cf51f8eb
Bryan921105 <bryan@stclab.com>
2023-11-15 00:30:26 +0900

    refactor(web-app): edit autoscaling history component and edit get history by from to api

--- core/web-app/src/app/app/autoscaling-history/page.tsx
+++ core/web-app/src/app/app/autoscaling-history/page.tsx
@@ -1,20 +1,17 @@
 'use client';
+import React, { useEffect, useMemo, useState } from 'react';
+import dayjs, { Dayjs } from 'dayjs';
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { decodeTime } from 'ulid';
 
 import ContentHeader from '../content-header';
 import AutoscalingHistoryService from '@/services/autoscaling-history';
-import dayjs, { Dayjs } from 'dayjs';
 import { AutoscalingHistoryDefinition } from '@/types/bindings/autoscaling-history-definition';
-import { renderKeyValuePairsWithJson } from '../keyvalue-renderer';
 import HistoryHeatmap from './history-heatmap';
-import { useSearchParams } from 'next/navigation';
-import { useRouter } from 'next/navigation';
-import { useEffect, useMemo, useState } from 'react';
-import { decodeTime } from 'ulid';
+import { renderKeyValuePairsWithJson } from '../keyvalue-renderer';
 
-async function getHistory(
-  from: Dayjs = dayjs().subtract(7, 'days'),
-  to: Dayjs = dayjs()
-) {
+async function getHistory(from: Dayjs, to: Dayjs) {
   const history = await AutoscalingHistoryService.getHistoryByFromTo(from, to);
   return history;
 }
@@ -26,28 +23,48 @@ interface AutoscalingHistoryDefinitionEx extends AutoscalingHistoryDefinition {
   created_at: number;
 }
 
-export default function AutoscalingHistoryPage({}) {
+export default function AutoscalingHistoryPage() {
   const searchParams = useSearchParams();
-  const from = searchParams.get('from');
-  const to = searchParams.get('to');
-  const fromDayjs = useMemo(() => (from ? dayjs(from) : DEFAULT_FROM), [from]);
-  const toDayjs = useMemo(() => (to ? dayjs(to) : DEFAULT_TO), [to]);
+  const from = searchParams.get('from') || DEFAULT_FROM.format('YYYY-MM-DD');
+  const to = searchParams.get('to') || DEFAULT_TO.format('YYYY-MM-DD');
+  const fromDayjs = useMemo(() => dayjs(from), [from]);
+  const toDayjs = useMemo(() => dayjs(to), [to]);
   const [history, setHistory] = useState<AutoscalingHistoryDefinitionEx[]>([]);
   const router = useRouter();
 
   useEffect(() => {
     const fetchHistory = async () => {
-      let history = await getHistory(fromDayjs, toDayjs);
-      history = history.map((historyItem: AutoscalingHistoryDefinition) => {
-        return {
+      try {
+        let history = await getHistory(fromDayjs, toDayjs);
+        history = history.map((historyItem: AutoscalingHistoryDefinition) => ({
           ...historyItem,
           created_at: decodeTime(historyItem.id),
-        };
-      });
-      setHistory(history);
+        }));
+        setHistory(history);
+      } catch (error) {
+        console.error({ error });
+      }
     };
     fetchHistory();
   }, [fromDayjs, toDayjs]);
+
+  const handleFromDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newFromDate = dayjs(e.target.value);
+    router.push(
+      `/app/autoscaling-history?from=${newFromDate.format(
+        'YYYY-MM-DD'
+      )}&to=${to}`
+    );
+  };
+
+  const handleToDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newToDate = dayjs(e.target.value);
+    router.push(
+      `/app/autoscaling-history?from=${from}&to=${newToDate.format(
+        'YYYY-MM-DD'
+      )}`
+    );
+  };
 
   return (
     <main className="h-full w-full">
@@ -60,16 +77,9 @@ export default function AutoscalingHistoryPage({}) {
                 <input
                   type="date"
                   className="input-bordered input input-sm max-w-[130px]"
-                  max={toDayjs?.format('YYYY-MM-DD')}
-                  defaultValue={fromDayjs?.format('YYYY-MM-DD')}
-                  onChange={(e) => {
-                    const from = dayjs(e.target.value);
-                    router.push(
-                      `/app/autoscaling-history?from=${from.format(
-                        'YYYY-MM-DD'
-                      )}&to=${toDayjs?.format('YYYY-MM-DD')}`
-                    );
-                  }}
+                  max={toDayjs.format('YYYY-MM-DD')}
+                  value={from}
+                  onChange={handleFromDateChange}
                 />
               </label>
             </div>
@@ -79,16 +89,9 @@ export default function AutoscalingHistoryPage({}) {
                 <input
                   type="date"
                   className="input-bordered input input-sm max-w-[130px]"
-                  min={fromDayjs?.format('YYYY-MM-DD')}
-                  defaultValue={toDayjs?.format('YYYY-MM-DD')}
-                  onChange={(e) => {
-                    const to = dayjs(e.target.value);
-                    router.push(
-                      `/app/autoscaling-history?from=${fromDayjs?.format(
-                        'YYYY-MM-DD'
-                      )}&to=${to.format('YYYY-MM-DD')}`
-                    );
-                  }}
+                  min={from}
+                  value={to}
+                  onChange={handleToDateChange}
                 />
               </label>
             </div>
@@ -98,104 +101,102 @@ export default function AutoscalingHistoryPage({}) {
       <div className="w-full">
         <HistoryHeatmap history={history} from={fromDayjs} to={toDayjs} />
         <table className="w-full">
-          <thead className="text-md flex h-12 w-full items-center justify-between border-b border-t bg-gray-200 px-8 py-0 font-bold text-gray-800 ">
-            <tr className="flex h-full w-full ">
+          <thead className="text-md flex h-12 w-full items-center justify-between border-b border-t bg-gray-200 px-8 py-0 font-bold text-gray-800">
+            <tr className="flex h-full w-full">
               <th className="mr-4 flex h-full items-center">
                 <label className="flex h-full items-center">
                   <input type="checkbox" className="checkbox" />
                 </label>
               </th>
               <th className="mx-4 flex h-full w-full items-center">
-                <div className="min-w-16 flex items-center ">Plan ID</div>
+                <div className="min-w-16 flex items-center">Plan ID</div>
               </th>
               <th className="mx-4 flex h-full w-full items-center">
-                <div className="min-w-20 flex items-center ">Plan Item</div>
+                <div className="min-w-20 flex items-center">Plan Item</div>
               </th>
               <th className="mx-4 flex h-full w-full items-center">
-                <div className="min-w-28 flex items-center ">Metric Values</div>
+                <div className="min-w-28 flex items-center">Metric Values</div>
               </th>
               <th className="mx-4 flex h-full w-full items-center">
-                <div className="min-w-36 flex items-center ">
+                <div className="min-w-36 flex items-center">
                   Metadata Values
                 </div>
               </th>
               <th className="mx-4 flex h-full w-full items-center">
-                <div className="min-w-28 flex items-center ">Fail Message</div>
+                <div className="min-w-28 flex items-center">Fail Message</div>
               </th>
               <th className="mx-4 flex h-full w-full items-center">
-                <div className="min-w-14 flex items-center ">Status</div>
+                <div className="min-w-14 flex items-center">Status</div>
               </th>
               <th className="mx-4 flex h-full w-full items-center">
-                <div className="min-w-12 flex items-center ">Date</div>
+                <div className="min-w-12 flex items-center">Date</div>
               </th>
             </tr>
           </thead>
-          <tbody className="text-md min-h-12 flex w-full items-center justify-between border-b border-t px-8 py-0 text-gray-800 ">
-            {history.map((historyItem: AutoscalingHistoryDefinitionEx) => {
-              return (
-                <tr key={historyItem.id} className="flex h-full w-full py-4">
-                  <td className="mr-4 flex h-full items-center">
-                    <label className="flex h-full items-center">
-                      <input type="checkbox" className="checkbox" />
-                    </label>
-                  </td>
-                  <td className="mx-4 flex h-full w-full items-start">
-                    <div className="min-w-16 flex items-center ">
-                      {historyItem.plan_id}
-                    </div>
-                  </td>
-                  <td className="mx-4 flex h-full w-full items-start">
-                    <div className="min-w-20 flex flex-col items-center ">
-                      {renderKeyValuePairsWithJson(
-                        historyItem.plan_item_json,
+          <tbody className="text-md min-h-12 flex w-full items-center justify-between border-b border-t px-8 py-0 text-gray-800">
+            {history.map((historyItem: AutoscalingHistoryDefinitionEx) => (
+              <tr key={historyItem.id} className="flex h-full w-full py-4">
+                <td className="mr-4 flex h-full items-center">
+                  <label className="flex h-full items-center">
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </td>
+                <td className="mx-4 flex h-full w-full items-start">
+                  <div className="min-w-16 flex items-center">
+                    {historyItem.plan_id}
+                  </div>
+                </td>
+                <td className="mx-4 flex h-full w-full items-start">
+                  <div className="min-w-20 flex flex-col items-center">
+                    {renderKeyValuePairsWithJson(
+                      historyItem.plan_item_json,
+                      false
+                    )}
+                  </div>
+                </td>
+                <td className="mx-4 flex h-full w-full items-start">
+                  <div className="min-w-28 flex flex-col items-center">
+                    {renderKeyValuePairsWithJson(
+                      historyItem.metric_values_json,
+                      false
+                    )}
+                  </div>
+                </td>
+                <td className="mx-4 flex h-full w-full items-start">
+                  <div className="min-w-36 flex flex-col items-center">
+                    {renderKeyValuePairsWithJson(
+                      historyItem.metadata_values_json,
+                      false
+                    )}
+                  </div>
+                </td>
+                <td className="mx-4 flex h-full w-full items-start">
+                  <div className="min-w-28 flex flex-col items-center">
+                    {historyItem.fail_message &&
+                      renderKeyValuePairsWithJson(
+                        historyItem.fail_message,
                         false
                       )}
-                    </div>
-                  </td>
-                  <td className="mx-4 flex h-full w-full items-start">
-                    <div className="min-w-28 flex flex-col items-center ">
-                      {renderKeyValuePairsWithJson(
-                        historyItem.metric_values_json,
-                        false
-                      )}
-                    </div>
-                  </td>
-                  <td className="mx-4 flex h-full w-full items-start">
-                    <div className="min-w-36 flex flex-col items-center ">
-                      {renderKeyValuePairsWithJson(
-                        historyItem.metadata_values_json,
-                        false
-                      )}
-                    </div>
-                  </td>
-                  <td className="mx-4 flex h-full w-full items-start">
-                    <div className="min-w-28 flex flex-col items-center ">
-                      {historyItem.fail_message &&
-                        renderKeyValuePairsWithJson(
-                          historyItem.fail_message,
-                          false
-                        )}
-                    </div>
-                  </td>
-                  <td className="mx-4 flex h-full w-full items-start">
-                    <div className="min-w-14 flex items-center ">
-                      {historyItem.fail_message ? (
-                        <div className="badge-error badge">Failed</div>
-                      ) : (
-                        <div className="badge-success badge">Success</div>
-                      )}
-                    </div>
-                  </td>
-                  <td className="mx-4 flex h-full w-full items-start">
-                    <div className="min-w-12 flex items-center  ">
-                      {dayjs
-                        .unix(historyItem.created_at / 1000)
-                        .format('YYYY-MM-DD HH:mm:ss')}
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
+                  </div>
+                </td>
+                <td className="mx-4 flex h-full w-full items-start">
+                  <div className="min-w-14 flex items-center">
+                    {historyItem.fail_message ? (
+                      <div className="badge-error badge">Failed</div>
+                    ) : (
+                      <div className="badge-success badge">Success</div>
+                    )}
+                  </div>
+                </td>
+                <td className="mx-4 flex h-full w-full items-start">
+                  <div className="min-w-12 flex items-center">
+                    {dayjs
+                      .unix(historyItem.created_at / 1000)
+                      .format('YYYY-MM-DD HH:mm:ss')}
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
