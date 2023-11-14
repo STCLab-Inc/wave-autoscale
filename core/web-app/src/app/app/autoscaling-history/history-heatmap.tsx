c41aaea67d966a95cded3182225504972979c6d7
Bryan921105 <bryan@stclab.com>
2023-11-15 01:02:14 +0900

    refactor(web-app): refactor history heat map component

--- core/web-app/src/app/app/autoscaling-history/history-heatmap.tsx
+++ core/web-app/src/app/app/autoscaling-history/history-heatmap.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { ResponsiveHeatMapCanvas } from '@nivo/heatmap';
 import dayjs, { Dayjs } from 'dayjs';
 import { groupBy } from 'lodash';
@@ -10,14 +8,16 @@ interface HistoryHeatmapProps {
   from: Dayjs;
   to: Dayjs;
 }
+
 function HistoryHeatmap({ history, from, to }: HistoryHeatmapProps) {
   const dataForHeatmap = useMemo(() => {
     if (!history) {
       return [];
     }
-    const groupedByDate = groupBy(history, (historyItem) => {
-      return dayjs.unix(historyItem.created_at / 1000).format('YYYY-MM-DD');
-    });
+
+    const groupedByDate = groupBy(history, (historyItem) =>
+      dayjs.unix(historyItem.created_at / 1000).format('YYYY-MM-DD')
+    );
 
     let current = from;
     while (current.isBefore(to) || current.isSame(to)) {
@@ -29,15 +29,12 @@ function HistoryHeatmap({ history, from, to }: HistoryHeatmapProps) {
     }
 
     const data = Object.entries(groupedByDate)
-      .sort(([dateA], [dateB]) => {
-        return dateA < dateB ? -1 : 1;
-      })
+      .sort(([dateA], [dateB]) => (dateA < dateB ? -1 : 1))
       .map(([date, historyItems]) => {
-        const groupedByHour = groupBy(historyItems, (historyItem) => {
-          return dayjs.unix(historyItem.created_at / 1000).format('HH');
-        });
+        const groupedByHour = groupBy(historyItems, (historyItem) =>
+          dayjs.unix(historyItem.created_at / 1000).format('HH')
+        );
 
-        // Fill in missing hours
         for (let i = 0; i < 24; i++) {
           const hour = i.toString().padStart(2, '0');
           if (!groupedByHour[hour]) {
@@ -62,6 +59,7 @@ function HistoryHeatmap({ history, from, to }: HistoryHeatmapProps) {
             }),
         };
       });
+
     return data;
   }, [history, from, to]);
 
@@ -80,16 +78,11 @@ function HistoryHeatmap({ history, from, to }: HistoryHeatmapProps) {
   return (
     <div className="h-80 w-full">
       <ResponsiveHeatMapCanvas
-        // forceSquare={true}
         data={dataForHeatmap}
-        /* xInnerPadding={0.1}
-        yInnerPadding={0.1} */
         margin={{ top: 50, right: 70, bottom: 30, left: 140 }}
         axisTop={{
           tickSize: 0,
           tickPadding: 10,
-          // tickRotation: -90,
-          /* legend: 'Time', */
           legendOffset: -40,
           legendPosition: 'middle',
         }}
@@ -97,9 +90,7 @@ function HistoryHeatmap({ history, from, to }: HistoryHeatmapProps) {
           tickSize: 0,
           tickPadding: 20,
           tickRotation: 0,
-          format: (value) => {
-            return value.replace(/-/g, '/');
-          },
+          format: (value) => value.replace(/-/g, '/'),
         }}
         colors={{
           type: 'diverging',
@@ -117,35 +108,24 @@ function HistoryHeatmap({ history, from, to }: HistoryHeatmapProps) {
           modifiers: [['brighter', 3]],
         }}
         annotations={[]}
-        tooltip={({ cell }) => {
-          return cell.data.z === 'Failed' ? (
-            <div className="bg-[#E0242E] p-2 text-white shadow-md">
-              <div className="text-xs">
-                Date: {cell.serieId.replace(/-/g, '/')}
-              </div>
-              <div className="text-xs">
-                Time: {cell.data.x}:00~{cell.data.x}:59
-              </div>
-              <div className="text-xs">Activity: {cell.data.y}</div>
-              {cell.data.z === 'Failed' ? (
-                <div className="text-xs">Status: {cell.data.z}</div>
-              ) : null}
+        tooltip={({ cell }) => (
+          <div
+            className={`bg-${
+              cell.data.z === 'Failed' ? '[#E0242E]' : '[#074eab]'
+            } p-2 text-white shadow-md`}
+          >
+            <div className="text-xs">
+              Date: {cell.serieId.replace(/-/g, '/')}
             </div>
-          ) : (
-            <div className="bg-[#074eab] p-2 text-white shadow-md">
-              <div className="text-xs">
-                Date: {cell.serieId.replace(/-/g, '/')}
-              </div>
-              <div className="text-xs">
-                Time: {cell.data.x}:00~{cell.data.x}:59
-              </div>
-              <div className="text-xs">Activity: {cell.data.y}</div>
-              {cell.data.z === 'Failed' ? (
-                <div className="text-xs">Status: {cell.data.z}</div>
-              ) : null}
+            <div className="text-xs">
+              Time: {cell.data.x}:00~{cell.data.x}:59
             </div>
-          );
-        }}
+            <div className="text-xs">Activity: {cell.data.y}</div>
+            {cell.data.z === 'Failed' ? (
+              <div className="text-xs">Status: {cell.data.z}</div>
+            ) : null}
+          </div>
+        )}
       />
     </div>
   );
