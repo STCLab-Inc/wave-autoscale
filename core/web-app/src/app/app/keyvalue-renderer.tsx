ce289e11bd8f2b7ba8ec4140150a4fb2b8376bd1
Bryan921105 <bryan@stclab.com>
2023-11-14 22:31:11 +0900

    feat(web-app): add indent flag to key value renderer utility

--- core/web-app/src/app/app/keyvalue-renderer.tsx
+++ core/web-app/src/app/app/keyvalue-renderer.tsx
@@ -2,19 +2,23 @@ import classNames from 'classnames';
 
 export function renderKeyValuePairs(
   keyValuePairs: { [key: string]: any },
+  indent?: boolean,
   depth = 0
 ) {
+  const indentFlag = indent === undefined || indent === true ? true : false;
+
   return Object.keys(keyValuePairs)
     .sort()
     .map((key) => {
       let value = keyValuePairs[key];
       if (value != null && typeof value === 'object') {
-        value = renderKeyValuePairs(value, 1);
+        value = renderKeyValuePairs(value, indentFlag, 1);
       }
       if (!value) {
         return null;
       }
-      return (
+
+      return indentFlag ? (
         <div
           key={key}
           className={classNames('mb-2', {
@@ -24,16 +28,37 @@ export function renderKeyValuePairs(
           <div className="whitespace-normal break-all font-bold">{key}</div>
           <div className="whitespace-normal break-all ">{value}</div>
         </div>
+      ) : (
+        <div
+          key={key}
+          className={classNames('mb-2 w-full', {
+            'ml-0': depth > 0,
+          })}
+        >
+          <div className="whitespace-normal break-all font-bold">{key}</div>
+          <div className="whitespace-normal break-all ">{value}</div>
+        </div>
       );
     });
 }
 
-export function renderKeyValuePairsWithJson(jsonString: string) {
+export function renderKeyValuePairsWithJson(
+  jsonString: string,
+  indent?: boolean
+) {
+  const indentFlag = indent === undefined || indent === true ? true : false;
+
   try {
     const keyValuePairs = JSON.parse(jsonString);
-    return renderKeyValuePairs(keyValuePairs);
+    return renderKeyValuePairs(keyValuePairs, indentFlag);
   } catch (error) {
     console.log({ error });
   }
-  return <div>{jsonString}</div>;
+  return indentFlag ? (
+    <div>{jsonString}</div>
+  ) : (
+    <div className={classNames('mb-2 w-full')}>
+      <div className="whitespace-normal break-all ">{jsonString}</div>
+    </div>
+  );
 }
