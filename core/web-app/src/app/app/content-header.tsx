c3ec9bd8d08966a5dc1bd187dabd8c16a3b556b6
Bryan921105 <bryan@stclab.com>
2023-11-17 20:17:48 +0900

    fix(web-app): edit content header design

--- core/web-app/src/app/app/content-header.tsx
+++ core/web-app/src/app/app/content-header.tsx
@@ -13,10 +13,10 @@ export default function ContentHeader({
 }: ContentHeaderProps) {
   return (
     <div className="flex h-14 w-full min-w-full flex-row items-center justify-between border-b border-t border-gray-200 bg-gray-75">
-      <div className="font-Pretendard whitespace-nowrap pl-8 pr-4 text-lg font-semibold text-gray-1000">
+      <div className="font-Pretendard whitespace-nowrap px-8 text-lg font-semibold text-gray-1000">
         {title}
       </div>
-      <div className="flex pl-4 pr-8">{right}</div>
+      <div className="flex px-8">{right}</div>
       {children}
     </div>
   );
