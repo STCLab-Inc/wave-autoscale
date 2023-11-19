1046cb8f473cbd925eda2f2d1a53dda213835297
Bryan921105 <bryan@stclab.com>
2023-11-20 02:37:30 +0900

    fix(web-app): revert layout for new metrics and scaling components

--- core/web-app/src/app/app/metrics/[id]/layout.tsx
+++ core/web-app/src/app/app/metrics/[id]/layout.tsx
@@ -1,6 +1,16 @@
 import MetricService from '@/services/metric';
 import MetricDetailDrawer from '../../metric-drawer';
+import { MetricDefinition } from '@/types/bindings/metric-definition';
 import MetricsPage from '../page';
+
+async function getMetricDefinition(dbId: string) {
+  try {
+    const metricDefinition = await MetricService.getMetric(dbId);
+    return metricDefinition;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 const NEW_PATH = 'new';
 
@@ -11,30 +21,15 @@ export default async function MetricDetailLayout({
   children: React.ReactNode;
   params: { id: string };
 }) {
-  if (dbId === NEW_PATH) {
-    return (
-      <div className="relative flex h-full w-full">
-        <MetricsPage />
-      </div>
-    );
-  }
-
-  try {
-    const metricDefinition = await MetricService.getMetric(dbId);
+  let metricDefinition: MetricDefinition | undefined;
+  if (dbId && dbId !== NEW_PATH) {
+    metricDefinition = await getMetricDefinition(dbId);
     console.log({ metricDefinition });
-
-    return (
-      <div className="relative flex h-full w-full">
-        <MetricsPage />
-        <MetricDetailDrawer metricDefinition={metricDefinition} />
-      </div>
-    );
-  } catch (error) {
-    console.error(error);
-    return (
-      <div className="relative flex h-full w-full">
-        <MetricsPage />
-      </div>
-    );
   }
+  return (
+    <div className="relative flex h-full w-full">
+      <MetricsPage />
+      <MetricDetailDrawer metricDefinition={metricDefinition} />;
+    </div>
+  );
 }
--- core/web-app/src/app/app/scaling-components/[id]/layout.tsx
+++ core/web-app/src/app/app/scaling-components/[id]/layout.tsx
@@ -11,34 +11,23 @@ export default async function ScalingComponentDetailLayout({
   children: React.ReactNode;
   params: { id: string };
 }) {
-  console.log(dbId);
-  if (dbId === NEW_PATH) {
-    return (
-      <div className="relative flex h-full w-full">
-        <ScalingComponentsPage />
-      </div>
-    );
+  let componentDefinition;
+
+  if (dbId && dbId !== NEW_PATH) {
+    try {
+      componentDefinition = await ScalingComponentService.getScalingComponent(
+        dbId
+      );
+      console.log({ componentDefinition });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  try {
-    const componentDefinition =
-      await ScalingComponentService.getScalingComponent(dbId);
-    console.log({ componentDefinition });
-
-    return (
-      <div className="relative flex h-full w-full">
-        <ScalingComponentsPage />
-        <ScalingComponentDetailDrawer
-          componentDefinition={componentDefinition}
-        />
-      </div>
-    );
-  } catch (error) {
-    console.error(error);
-    return (
-      <div className="relative flex h-full w-full">
-        <ScalingComponentsPage />
-      </div>
-    );
-  }
+  return (
+    <div className="relative flex h-full w-full">
+      <ScalingComponentsPage />
+      <ScalingComponentDetailDrawer componentDefinition={componentDefinition} />
+    </div>
+  );
 }
