8b0cf19ee21a0013d95c82eec8fee4b884e8d6d3
Bryan921105 <bryan@stclab.com>
2023-11-19 21:15:11 +0900

    feat(web-app): add plan drawer removing from sidebar

--- core/web-app/src/app/app/planning/sidebar.tsx
+++ core/web-app/src/app/app/planning/sidebar.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useParams } from 'next/navigation';
+import { useParams, usePathname } from 'next/navigation';
 import Link from 'next/link';
 import classNames from 'classnames';
 
 import PlanService from '@/services/plan';
 import { ScalingPlanDefinition } from '@/types/bindings/scaling-plan-definition';
-import { generatePlanDefinition } from '@/utils/plan-binding';
 import { usePlanStore } from './plan-store';
 
 interface ScalingPlanDefinitionEx extends ScalingPlanDefinition {
@@ -16,18 +14,17 @@ interface ScalingPlanDefinitionEx extends ScalingPlanDefinition {
 }
 
 export default function PlanningSidebar() {
-  const [addModalToggle, setAddModalToggle] = useState(false);
   const needToSave = usePlanStore((state) => state.needToSave);
   const currentScalingPlanState = usePlanStore(
     (state) => state.currentScalingPlanState
   );
   console.log({ currentScalingPlanState });
 
-  const [timestamp, setTimestamp] = useState(Date.now());
-  const { register, reset, setFocus, handleSubmit } = useForm();
   const [plans, setPlans] = useState([]);
   const params = useParams();
   const selectedPlanId = params?.id;
+
+  const pathname = usePathname();
 
   useEffect(() => {
     const fetchPlans = async () => {
@@ -35,36 +32,7 @@ export default function PlanningSidebar() {
       setPlans(response);
     };
     fetchPlans();
-  }, [timestamp]);
-
-  const onClickAdd = () => {
-    reset();
-    setAddModalToggle(true);
-    // TODO: Fix this hack
-    setTimeout(() => {
-      setFocus('title');
-    }, 100);
-  };
-
-  const onSubmitAddDialog = async (data: any) => {
-    const { title } = data;
-    if (!title?.trim()) {
-      return;
-    }
-    const plan = generatePlanDefinition({ title });
-    try {
-      const response = await PlanService.createPlan(plan);
-      setTimestamp(Date.now());
-      setAddModalToggle(false);
-    } catch (e) {}
-  };
-  const onClickCancelInModal = (event: any) => {
-    // Check if this event is from the element itself
-    if (event.target !== event.currentTarget) {
-      return;
-    }
-    setAddModalToggle(false);
-  };
+  }, [pathname]);
 
   console.log(plans);
 
@@ -75,13 +43,13 @@ export default function PlanningSidebar() {
           <span className="font-Pretendard whitespace-nowrap text-lg font-semibold text-gray-1000">
             Plans {plans !== undefined ? `(${plans.length})` : undefined}
           </span>
-
-          <button
-            className="flex h-8 items-center justify-center rounded-md border border-gray-600 bg-gray-50 pl-5 pr-5 text-sm text-gray-600"
-            onClick={onClickAdd}
-          >
-            ADD
-          </button>
+          <div className="flex items-center">
+            <Link href="/app/planning/new">
+              <button className="flex h-8 items-center justify-center rounded-md border border-blue-400 bg-blue-400  pl-5 pr-5 text-sm text-gray-50">
+                ADD PLAN
+              </button>
+            </Link>
+          </div>
         </div>
         <div className="flex flex-col">
           {plans?.map((plan: ScalingPlanDefinitionEx) => (
@@ -109,51 +77,6 @@ export default function PlanningSidebar() {
           ))}
         </div>
       </aside>
-
-      <div
-        className={classNames('modal', { 'modal-open': addModalToggle })}
-        onClick={onClickCancelInModal}
-      >
-        <div className="modal-box">
-          <form onSubmit={handleSubmit(onSubmitAddDialog)}>
-            <label className="m-0 flex w-full pb-4">
-              <span className="font-Pretendard text-md whitespace-nowrap font-semibold text-gray-1000">
-                Add a Plan
-              </span>
-            </label>
-
-            <div className="form-control w-full py-2">
-              <label className="label px-0 py-2">
-                <span className="text-md label-text px-2">Plan Title</span>
-                {/* <span className="label-text-alt">label-text-alt</span> */}
-              </label>
-
-              <input
-                {...register('title')}
-                type="text"
-                placeholder="Plan Title"
-                className="input-bordered input my-2 w-full px-4 text-sm focus:outline-none"
-              />
-            </div>
-
-            <div className="modal-action m-0 pt-4">
-              <button
-                className="flex h-8 items-center justify-center rounded-md border border-gray-600 pl-5 pr-5 text-sm text-gray-600"
-                onClick={onClickCancelInModal}
-              >
-                CANCEL
-              </button>
-
-              <button
-                className="flex h-8 items-center justify-center rounded-md border border-blue-400 bg-blue-400 pl-5 pr-5 text-sm text-gray-50"
-                type="submit"
-              >
-                ADD
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
     </div>
   );
 }
