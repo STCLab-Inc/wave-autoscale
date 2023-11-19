f06768b771bed799c0d26f68c05e663cf7811339
Bryan921105 <bryan@stclab.com>
2023-11-19 19:18:08 +0900

    feat(web-app): add scaling component drawer component

--- core/web-app/src/app/app/scaling-component-drawer.tsx
+++ core/web-app/src/app/app/scaling-component-drawer.tsx
@@ -4,8 +4,8 @@ import React, { useEffect, useMemo, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { forEach } from 'lodash';
+import Image from 'next/image';
 
-import MetricService from '@/services/metric';
 import { ScalingComponentDefinition } from '@/types/bindings/scaling-component-definition';
 import {
   generateScalingComponentDefinition,
@@ -14,7 +14,7 @@ import {
 import ScalingComponentService from '@/services/scaling-component';
 import { getMetadataFormControls } from './metadata-form-controls';
 
-// Metric Types
+// Scaling Component Types
 const componentKeyTypes = getScalingComponentKeyTypes();
 const componentOptions = componentKeyTypes.map((componentKeyType) => (
   <option
@@ -98,6 +98,21 @@ export default function ScalingComponentDetailDrawer({
     setValue('component_kind', componentKind);
   };
 
+  // Exit scaling-components drawer
+  const onClickExit = async () => {
+    goBack();
+  };
+
+  const onClickInitialize = async () => {
+    setSelectedComponentKind('Scaling Component Kind');
+
+    // Clear metadata
+    const id = getValues('id');
+    reset();
+    setValue('id', id);
+    setValue('component_kind', 'Scaling Component Kind');
+  };
+
   const onClickRemove = async () => {
     if (isNew || !dbId) {
       return;
@@ -120,13 +135,11 @@ export default function ScalingComponentDetailDrawer({
     });
     try {
       if (isNew) {
-        // const result = await MetricService.createMetric(componentDefinition);
         const result = await ScalingComponentService.createScalingComponent(
           componentDefinition
         );
         console.log({ result, isNew });
       } else {
-        // const result = await MetricService.updateMetric(componentDefinition);
         const result = await ScalingComponentService.updateScalingComponent(
           componentDefinition
         );
@@ -139,41 +152,74 @@ export default function ScalingComponentDetailDrawer({
   };
 
   return (
-    <div className="scaling-components-drawer drawer drawer-end absolute bottom-0 top-0 z-50 border-l border-t border-gray-200">
+    <div className="scaling-components-drawer drawer drawer-end fixed bottom-0 right-0 top-16 z-50 w-full border-t border-gray-200">
       <input id="drawer" type="checkbox" className="drawer-toggle" checked />
-      <div className="drawer-side">
+      <div className="drawer-side h-full border-t border-gray-200">
         <label
           htmlFor="drawer"
           className="drawer-overlay"
           onClick={onClickOverlay}
         />
-        <div className="drawer-content min-w-[32rem] overflow-y-auto bg-base-100 p-4">
+        <div className="drawer-content flex h-full min-w-[25rem] flex-col overflow-y-auto border-l border-gray-200 bg-base-100 pb-20">
           <form className="" onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-4 flex items-center justify-between">
-              <h2 className="font-bold">Scaling Component</h2>
-              <div>
-                {isNew ? undefined : (
+            <div className="flex h-14 min-h-14 w-full min-w-full flex-row items-center justify-between border-b border-dashed border-gray-400 bg-gray-75">
+              <span className="font-Pretendard truncate whitespace-nowrap px-4 text-lg font-semibold text-gray-1000">
+                Scaling Component
+              </span>
+              <div className="flex px-4">
+                {isNew ? (
                   <button
-                    type="button"
-                    className="btn-error btn-sm btn mr-2"
+                    className="mr-1 flex h-8 items-center justify-center rounded-md border border-red-400  bg-red-400 pl-1 pr-1 text-sm text-gray-50"
+                    onClick={onClickInitialize}
+                  >
+                    <Image
+                      src="/assets/icons/initialize.svg"
+                      alt="initialize.svg"
+                      priority={true}
+                      width={24}
+                      height={24}
+                      style={{ minWidth: '1.5rem', maxWidth: '1.5rem' }}
+                    />
+                  </button>
+                ) : (
+                  <button
+                    className="mr-1 flex h-8 items-center justify-center rounded-md border border-red-400  bg-red-400 pl-1 pr-1 text-sm text-gray-50"
                     onClick={onClickRemove}
                   >
-                    Remove
+                    <Image
+                      src="/assets/icons/delete.svg"
+                      alt="delete.svg"
+                      priority={true}
+                      width={24}
+                      height={24}
+                      style={{ minWidth: '1.5rem', maxWidth: '1.5rem' }}
+                    />
                   </button>
                 )}
-
-                <button type="submit" className="btn-primary btn-sm btn">
-                  Save
+                <button
+                  className="ml-1 mr-1 flex h-8 items-center justify-center rounded-md border border-blue-400 bg-blue-400 pl-5 pr-5 text-sm text-gray-50"
+                  type="submit"
+                >
+                  SAVE
+                </button>
+                <button
+                  className="ml-1 flex h-8 items-center justify-center rounded-md border border-gray-600 pl-5 pr-5 text-sm text-gray-600"
+                  onClick={onClickExit}
+                >
+                  EXIT
                 </button>
               </div>
             </div>
-            <div className="form-control mb-4 w-full">
-              <label className="label">
-                <span className="label-text">Scaling Component Type</span>
-                <span className="label-text-alt"></span>
+
+            <div className="form-control w-full px-4 py-2">
+              <label className="label px-0 py-2">
+                <span className="text-md label-text px-2">
+                  Scaling Component Type
+                </span>
+                {/* <span className="label-text-alt">label-text-alt</span> */}
               </label>
               <select
-                className="select-bordered select"
+                className="select-bordered select my-2 flex w-full truncate rounded-md text-sm focus:outline-none"
                 defaultValue="Scaling Component Kind"
                 {...register('component_kind', {
                   required: true,
@@ -184,15 +230,21 @@ export default function ScalingComponentDetailDrawer({
                 {componentOptions}
               </select>
             </div>
-            <div className="form-control mb-4 w-full">
-              <label className="label">
-                <span className="label-text">ID</span>
-                <span className="label-text-alt">used as a variable name</span>
+
+            <div className="form-control w-full px-4 py-2">
+              <label className="label px-0 py-2">
+                <span className="text-md label-text px-2">
+                  Scaling Component ID
+                </span>
+                {/* <span className="label-text-alt">label-text-alt</span> */}
               </label>
               <input
                 type="text"
-                placeholder="Type here"
-                className="input-bordered input w-full"
+                placeholder="Scaling Component ID"
+                className="input-bordered input my-2 w-full px-4 text-sm focus:outline-none"
+                autoComplete="off"
+                autoCapitalize="off"
+                autoCorrect="off"
                 {...register('id', { required: true })}
               />
             </div>
