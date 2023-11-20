30b323a4393bd4650bb47d8a356d86811564a486
Bryan921105 <bryan@stclab.com>
2023-11-21 04:20:23 +0900

    feat(web-app): add pagination for metrics and url routing with modal

--- core/web-app/src/app/app/metrics/page.tsx
+++ core/web-app/src/app/app/metrics/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
+import { useSearchParams, useRouter } from 'next/navigation';
 
 import ContentHeader from '../content-header';
 import { MetricDefinition } from '@/types/bindings/metric-definition';
 import MetricService from '@/services/metric';
 import { renderKeyValuePairsWithJson } from '../keyvalue-renderer';
+import MetricDetailDrawer from '../metric-drawer';
 
 async function fetchMetrics() {
   try {
@@ -23,6 +24,14 @@ interface MetricDefinitionEx extends MetricDefinition {
 }
 
 export default function MetricsPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const pageParam = searchParams.get('page');
+  const page = pageParam ? parseInt(pageParam, 10) : 1;
+  const viewParam = searchParams.get('view');
+  const view = viewParam ? parseInt(viewParam, 10) : 10;
+
   const [metrics, setMetrics] = useState<MetricDefinitionEx[]>([]);
   const [checkAllFlag, setCheckAllFlag] = useState(false);
 
@@ -49,7 +58,7 @@ export default function MetricsPage() {
   const [itemsPerPage, setItemsPerPage] = useState(ITEMS_PER_PAGE_OPTIONS[0]);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const totalPageCount = Math.ceil(metrics.length / itemsPerPage);
+  const totalPageCount = Math.ceil(metrics.length / itemsPerPage) || 1;
 
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
@@ -67,6 +76,36 @@ export default function MetricsPage() {
     setCurrentPage(newPage);
   };
 
+  useEffect(() => {
+    setCurrentPage(page ? page : 1);
+    setItemsPerPage(view ? view : ITEMS_PER_PAGE_OPTIONS[0]);
+  }, [page, view]);
+
+  useEffect(() => {
+    if (currentPage > totalPageCount) {
+      setCurrentPage(1);
+    }
+    router.push(`/app/metrics?page=${currentPage}&view=${itemsPerPage}`);
+  }, [currentPage, itemsPerPage]);
+
+  const [detailsModalFlag, setDetailsModalFlag] = useState(false);
+  const [metricsItem, setMetricsItem] = useState<MetricDefinitionEx>();
+
+  const onClickDetails = (metricsItem: MetricDefinitionEx | undefined) => {
+    setMetricsItem(metricsItem);
+    setDetailsModalFlag(true);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const metricsData = await fetchMetrics();
+      setMetrics(metricsData);
+    }
+    if (!detailsModalFlag) {
+      fetchData();
+    }
+  }, [detailsModalFlag]);
+
   return (
     <main className="flex h-full w-full flex-col">
       <div>
@@ -74,11 +113,12 @@ export default function MetricsPage() {
           title="Metrics"
           right={
             <div className="flex items-center">
-              <Link href="/app/metrics/new">
-                <button className="flex h-8 items-center justify-center rounded-md border border-blue-400 bg-blue-400  pl-5 pr-5 text-sm text-gray-50">
-                  ADD METRIC
-                </button>
-              </Link>
+              <button
+                className="flex h-8 items-center justify-center rounded-md border border-blue-400 bg-blue-400  pl-5 pr-5 text-sm text-gray-50"
+                onClick={() => onClickDetails(undefined)}
+              >
+                ADD METRIC
+              </button>
             </div>
           }
         />
@@ -110,8 +150,8 @@ export default function MetricsPage() {
               <button
                 className={
                   currentPage === 1
-                    ? 'ml-1 mr-1 flex h-8 cursor-not-allowed items-center justify-center rounded-md border border-gray-400 bg-gray-400 pl-5 pr-5 text-sm text-gray-50'
-                    : 'ml-1 mr-1 flex h-8 cursor-pointer items-center justify-center rounded-md border border-red-400 bg-red-400 pl-5 pr-5 text-sm text-gray-50'
+                    ? 'mr-1 flex h-8 cursor-not-allowed items-center justify-center rounded-md border border-gray-400 bg-gray-400 pl-5 pr-5 text-sm text-gray-50'
+                    : 'mr-1 flex h-8 cursor-pointer items-center justify-center rounded-md border border-red-400 bg-red-400 pl-5 pr-5 text-sm text-gray-50'
                 }
                 onClick={() => handlePageChange(currentPage - 1)}
                 disabled={currentPage === 1}
@@ -123,8 +163,8 @@ export default function MetricsPage() {
                   currentPage &&
                   totalPageCount &&
                   currentPage !== totalPageCount
-                    ? 'ml-1 mr-1 flex h-8 cursor-pointer items-center justify-center rounded-md border border-blue-400 bg-blue-400 pl-5 pr-5 text-sm text-gray-50'
-                    : 'ml-1 mr-1 flex h-8 cursor-not-allowed items-center justify-center rounded-md border border-gray-400 bg-gray-400 pl-5 pr-5 text-sm text-gray-50'
+                    ? 'ml-1 flex h-8 cursor-pointer items-center justify-center rounded-md border border-blue-400 bg-blue-400 pl-5 pr-5 text-sm text-gray-50'
+                    : 'ml-1 flex h-8 cursor-not-allowed items-center justify-center rounded-md border border-gray-400 bg-gray-400 pl-5 pr-5 text-sm text-gray-50'
                 }
                 onClick={() => handlePageChange(currentPage + 1)}
                 disabled={currentPage === totalPageCount}
@@ -147,22 +187,22 @@ export default function MetricsPage() {
                     />
                   </label>
                 </th>
-                <th className="mx-4 flex h-full w-full flex-8 items-center">
-                  <span className="flex items-center break-words">
-                    Metric Kind
-                  </span>
-                </th>
-                <th className="mx-4 flex h-full w-full flex-8 items-center">
+                <th className="mx-4 flex h-full w-full flex-4 items-center">
                   <span className="flex items-center break-words">
                     Metric ID
                   </span>
                 </th>
-                <th className="mx-4 flex h-full w-full flex-8 items-center">
+                <th className="mx-4 flex h-full w-full flex-2 items-center">
+                  <span className="flex items-center break-words">
+                    Collector
+                  </span>
+                </th>
+                <th className="mx-4 flex h-full w-full flex-10 items-center">
                   <span className="flex items-center break-words">
                     Metadata Values
                   </span>
                 </th>
-                <th className="mx-4 flex h-full w-full flex-2 items-center">
+                <th className="mx-4 flex h-full w-full flex-1 items-center">
                   <span className="flex items-center break-words">Actions</span>
                 </th>
               </tr>
@@ -193,17 +233,17 @@ export default function MetricsPage() {
                       </label>
                     </td>
 
-                    <td className="mx-4 flex h-full w-full flex-8 items-start">
-                      <div className="flex items-center break-all">
-                        {metricsItem.kind}
-                      </div>
-                    </td>
-                    <td className="mx-4 flex h-full w-full flex-8 items-start">
+                    <td className="mx-4 flex h-full w-full flex-4 items-start">
                       <div className="flex items-center break-all">
                         {metricsItem.id}
                       </div>
                     </td>
-                    <td className="mx-4 flex h-full w-full flex-8 items-start">
+                    <td className="mx-4 flex h-full w-full flex-2 items-start">
+                      <div className="flex items-center break-all">
+                        {metricsItem.collector}
+                      </div>
+                    </td>
+                    <td className="mx-4 flex h-full w-full flex-10 items-start">
                       <div className="flex flex-col items-center">
                         {renderKeyValuePairsWithJson(
                           JSON.stringify(metricsItem.metadata),
@@ -211,13 +251,14 @@ export default function MetricsPage() {
                         )}
                       </div>
                     </td>
-                    <td className="mx-4 flex h-full w-full flex-2 items-start">
+                    <td className="mx-4 flex h-full w-full flex-1 items-start">
                       <div className="flex items-center">
-                        <Link href={`/app/metrics/${metricsItem.db_id}`}>
-                          <button className="badge-success badge bg-[#074EAB] px-2 py-3 text-white">
-                            Details
-                          </button>
-                        </Link>
+                        <button
+                          className="badge-info badge bg-[#99E3D0] px-2 py-3 text-white"
+                          onClick={() => onClickDetails(metricsItem)}
+                        >
+                          Details
+                        </button>
                       </div>
                     </td>
                   </tr>
@@ -227,6 +268,12 @@ export default function MetricsPage() {
           </table>
         </div>
       </div>
+      {detailsModalFlag ? (
+        <MetricDetailDrawer
+          metricDefinition={metricsItem}
+          exitFunction={() => setDetailsModalFlag(false)}
+        />
+      ) : null}
     </main>
   );
 }
