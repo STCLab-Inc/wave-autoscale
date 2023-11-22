6364edd84bc0d37ad5f672ed50b5516c458bcd4c
Bryan921105 <bryan@stclab.com>
2023-11-23 03:18:22 +0900

    feat(web-app): add features and refactor sidebar of planning page

--- core/web-app/src/app/app/planning/sidebar.tsx
+++ core/web-app/src/app/app/planning/sidebar.tsx
@@ -1,36 +1,42 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useParams, usePathname } from 'next/navigation';
-import Link from 'next/link';
 import classNames from 'classnames';
 
 import PlanService from '@/services/plan';
 import { ScalingPlanDefinition } from '@/types/bindings/scaling-plan-definition';
-import { usePlanStore } from './plan-store';
 
 interface ScalingPlanDefinitionEx extends ScalingPlanDefinition {
   metadata: { cool_down: number; interval: number; title: string };
 }
 
-export default function PlanningSidebar() {
-  const needToSave = usePlanStore((state) => state.needToSave);
-  const currentScalingPlanState = usePlanStore(
-    (state) => state.currentScalingPlanState
-  );
+export default function PlanningSidebar({
+  plans,
+  plansItem,
+  setPlansItem,
+  detailsModalFlag,
+  setDetailsModalFlag,
+  setFetchFlag,
+}: {
+  plans: ScalingPlanDefinitionEx[];
+  plansItem: ScalingPlanDefinitionEx | undefined;
+  setPlansItem: (plan: ScalingPlanDefinitionEx | undefined) => void;
+  detailsModalFlag: boolean;
+  setDetailsModalFlag: (detailsModalFlag: boolean) => void;
+  setFetchFlag: (fetchFlag: boolean) => void;
+}) {
+  const onClickDetails = (planItem: ScalingPlanDefinitionEx | undefined) => {
+    setPlansItem(planItem);
+    setDetailsModalFlag(true);
+  };
 
-  const [plans, setPlans] = useState<ScalingPlanDefinitionEx[]>([]);
-  const params = useParams();
-  const selectedPlanId = params?.id;
-  const pathname = usePathname();
-
-  useEffect(() => {
-    const fetchPlans = async () => {
-      const response = await PlanService.getPlans();
-      setPlans(response);
-    };
-    fetchPlans();
-  }, [pathname]);
+  const onClickMenu = (plan: ScalingPlanDefinitionEx | undefined) => {
+    if (plansItem === plan) {
+      setPlansItem(undefined);
+    } else {
+      setPlansItem(plan);
+    }
+  };
 
   return (
     <div className="flex h-full">
@@ -40,36 +46,37 @@ export default function PlanningSidebar() {
             Plans ({plans.length})
           </span>
           <div className="flex items-center">
-            <Link href="/app/planning/new">
-              <button className="flex h-8 items-center justify-center rounded-md border border-blue-400 bg-blue-400  pl-5 pr-5 text-sm text-gray-50">
-                ADD PLAN
-              </button>
-            </Link>
+            <button
+              className="flex h-8 items-center justify-center rounded-md border border-blue-400 bg-blue-400  pl-5 pr-5 text-sm text-gray-50"
+              onClick={() => onClickDetails(undefined)}
+              type="button"
+            >
+              ADD PLAN
+            </button>
           </div>
         </div>
         <div className="flex flex-col">
           {plans.map((plan: ScalingPlanDefinitionEx) => (
-            <Link
-              href={`/app/planning/${plan.db_id}`}
+            <button
               key={plan.db_id}
+              onClick={() => onClickMenu(plan)}
               className={classNames(
                 'flex h-12 w-full cursor-pointer items-center border-b px-8',
                 {
                   'hover:bg-blue-100 hover:text-blue-600':
-                    plan.db_id !== selectedPlanId,
-                  'bg-primary': plan.db_id === selectedPlanId,
+                    plan.db_id !== plansItem?.db_id,
+                  'bg-primary': plan.db_id === plansItem?.db_id,
                 }
               )}
             >
               <span
                 className={classNames('truncate', {
-                  'text-white': plan.db_id === selectedPlanId,
+                  'text-white': plan.db_id === plansItem?.db_id,
                 })}
               >
-                {plan.metadata.title}
-                {currentScalingPlanState && needToSave(plan.db_id) && '*'}
+                {plan.id}
               </span>
-            </Link>
+            </button>
           ))}
         </div>
       </aside>
