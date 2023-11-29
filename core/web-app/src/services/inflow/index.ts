080cc09e51f1130d7a602081599d777dc6b7f8fe
Bryan921105 <bryan@stclab.com>
2023-11-29 19:56:28 +0900

    refactor(web-app): edit api of inflow related codes

--- core/web-app/src/services/inflow/index.ts
+++ core/web-app/src/services/inflow/index.ts
@@ -2,25 +2,17 @@ import { DataLayer } from '@/infra/data-layer';
 import { Dayjs } from 'dayjs';
 
 class InflowServiceClass {
-  async getInflowByFromToInDatabase(from: Dayjs, to: Dayjs) {
-    const response = await DataLayer.get(
-      `/api/inflow/database?from=${
-        from.format('YYYY-MM-DDTHH:mm:ss') + '.000Z'
-      }&to=${to.format('YYYY-MM-DDTHH:mm:ss') + '.000Z'}`
-    );
+  async getInflowMetricId() {
+    const response = await DataLayer.get(`/api/inflow/metric_id`);
     return response.data;
   }
-  async getInflowMetricIdInMemory() {
-    const response = await DataLayer.get(`/api/inflow/memory/metric_id`);
-    return response.data;
-  }
-  async getInflowWithMetricIdByFromToInMemory(
+  async getInflowWithMetricIdByDate(
     metric_id: String,
     from: Dayjs,
     to: Dayjs
   ) {
     const response = await DataLayer.get(
-      `/api/inflow/memory?metric_id=${metric_id}&from=${
+      `/api/inflow?metric_id=${metric_id}&from=${
         from.format('YYYY-MM-DDTHH:mm:ss') + '.000Z'
       }&to=${to.format('YYYY-MM-DDTHH:mm:ss') + '.000Z'}`
     );
