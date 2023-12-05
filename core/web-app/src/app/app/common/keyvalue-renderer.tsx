b59077c822013087c733649822e31d115e6a2fe1
Bryan921105 <bryan@stclab.com>
2023-12-06 00:09:12 +0900

    refactor(web-app): edit key value renderer component

--- core/web-app/src/app/app/common/keyvalue-renderer.tsx
+++ core/web-app/src/app/app/common/keyvalue-renderer.tsx
@@ -27,15 +27,15 @@ export function renderKeyValuePairs(
     >
       {Object.keys(keyValuePairs)
         .sort()
-        .flatMap((key) => {
+        .flatMap((key, index) => {
           const value = keyValuePairs[key];
 
           if (value != null && typeof value === 'object') {
             return Array.isArray(keyValuePairs) ? (
               renderKeyValuePairs(value, indentFlag, depth + 1, true)
             ) : (
-              <div key={key}>
-                <div className="whitespace-normal break-all font-bold">
+              <div key={index} className="flex w-full flex-col">
+                <div className="flex w-full flex-col whitespace-normal break-all font-bold">
                   {key}
                 </div>
                 {renderKeyValuePairs(value, indentFlag, depth + 1)}
@@ -48,13 +48,19 @@ export function renderKeyValuePairs(
           }
 
           return Array.isArray(keyValuePairs) ? (
-            <div key={key}>
-              <div className="whitespace-normal break-all">{value}</div>
+            <div key={key} className="flex w-full flex-col">
+              <span className="flex w-full flex-col whitespace-normal break-all">
+                {value}
+              </span>
             </div>
           ) : (
-            <div key={key}>
-              <div className="whitespace-normal break-all font-bold">{key}</div>
-              <div className="whitespace-normal break-all">{value}</div>
+            <div key={key} className="flex w-full flex-col">
+              <span className="hitespace-normal flex w-full flex-col break-all font-bold">
+                {key}
+              </span>
+              <span className="flex w-full flex-col whitespace-normal break-all">
+                {value}
+              </span>
             </div>
           );
         })}
@@ -63,23 +69,27 @@ export function renderKeyValuePairs(
 }
 
 export function renderKeyValuePairsWithJson(
-  jsonString: string,
+  data: object | string,
   indent?: boolean
 ): React.ReactNode {
   const indentFlag = indent ?? true;
 
+  if (typeof data === 'object') {
+    data = JSON.stringify(data);
+  }
+
   try {
-    const keyValuePairs = JSON.parse(jsonString);
+    const keyValuePairs = JSON.parse(data);
     return renderKeyValuePairs(keyValuePairs, indentFlag);
   } catch (error) {
     /* console.error({ error }); */
   }
 
   return indentFlag ? (
-    <div>{jsonString}</div>
+    <span>{data}</span>
   ) : (
     <div className="w-full pb-2">
-      <div className="whitespace-normal break-all ">{jsonString}</div>
+      <div className="whitespace-normal break-all ">{data}</div>
     </div>
   );
 }
