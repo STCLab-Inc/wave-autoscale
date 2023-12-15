0ed247b97465e1482bd9604a1673ebb006e94f2d
Hwansoo Kim <pueding@gmail.com>
2023-12-15 16:49:02 +0900

    fix(web-app): change the menu layout

--- core/web-app/src/app/app/common/menu.tsx
+++ core/web-app/src/app/app/common/menu.tsx
@@ -1,41 +1,54 @@
 'use client';
 
 import React from 'react';
-
 import classNames from 'classnames';
-import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-type MenuItemType = 'UNFOLD' | 'FOLD';
+interface MenuItem {
+  targetPath?: string;
+  label?: string;
+  spacer?: boolean;
+}
 
-interface MenuItemProps {
-  type?: MenuItemType;
+const MENU_ITEMS: MenuItem[] = [
+  { targetPath: '/app', label: 'Dashboard' },
+  { targetPath: '/app/autoscaling-history', label: 'Autoscaling Logs' },
+  { targetPath: '/app/inflow', label: 'Metrics Inflow' },
+  { spacer: true },
+  { targetPath: '/app/scaling-plans', label: 'Scaling Plans' },
+  { targetPath: '/app/metrics', label: 'Metrics' },
+  { targetPath: '/app/scaling-components', label: 'Scaling Components' },
+  { spacer: true },
+  {
+    targetPath: 'https://github.com/STCLab-Inc/wave-autoscale',
+    label: 'Github',
+  },
+];
+
+interface MenuButtonProps {
+  menuItem: MenuItem;
   isActive: boolean;
-  targetPath: string;
-  label: string;
   onClick?: () => void;
 }
 
-function MenuItem({
-  type = 'UNFOLD',
-  isActive,
-  targetPath,
-  label,
-  onClick,
-}: MenuItemProps) {
+function MenuButton({ menuItem, isActive, onClick }: MenuButtonProps) {
+  if (menuItem.spacer) {
+    return <li className="mx-6 h-4 w-[1px] bg-gray-200" />;
+  }
+  const targetPath = menuItem.targetPath || '';
+  const label = menuItem.label || '';
   return (
     <li
       className={classNames(
-        'flex-column mx-0.5 flex h-full items-center lg:flex-row',
+        'flex-column mr-4 flex h-full items-center lg:flex-row',
         isActive ? 'border-b-4 border-purple-500' : 'border-b-4 border-white'
       )}
     >
       <Link
         className={classNames(
-          isActive ? 'text-gray-1000' : 'text-gray-600',
-          'whitespace-nowrap px-4',
-          type === 'UNFOLD' ? '' : type === 'FOLD' ? ' py-2' : ''
+          'whitespace-nowrap px-4 tracking-tight',
+          isActive ? 'text-gray-1000' : 'text-gray-600'
         )}
         href={targetPath}
         onClick={onClick}
@@ -47,77 +60,57 @@ function MenuItem({
   );
 }
 
-type MenuType = 'UNFOLD' | 'FOLD';
-
 interface MenuProps {
-  type?: MenuType;
-  windowWidth: number;
-  menuFlag?: boolean;
-  setMenuFlag: React.Dispatch<React.SetStateAction<boolean>>;
+  onClickMenu: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const menuItems = [
-  { targetPath: '/app/autoscaling-history', label: 'Autoscaling History' },
-  { targetPath: '/app/scaling-plans', label: 'Scaling Plans' },
-  { targetPath: '/app/metrics', label: 'Metrics' },
-  { targetPath: '/app/scaling-components', label: 'Scaling Components' },
-  { targetPath: '/app/inflow', label: 'Metrics Inflow' },
-  {
-    targetPath: 'https://github.com/STCLab-Inc/wave-autoscale',
-    label: 'Github',
-  },
-];
-
-export default function Menu({
-  type,
-  windowWidth,
-  menuFlag,
-  setMenuFlag,
-}: MenuProps) {
+export default function Menu() {
   const pathname = usePathname();
 
-  return windowWidth < 880 ? (
-    type === 'UNFOLD' ? (
-      <ul className="flex h-full w-full flex-row items-center justify-end pt-1 lg:flex-row">
-        <figure
-          onClick={() => setMenuFlag((menuFlag) => !menuFlag)}
-          className="cursor-pointer"
-        >
-          <Image
-            src="/assets/icons/menu.svg"
-            alt="menu.svg"
-            priority={true}
-            width={36}
-            height={32}
-            style={{
-              minWidth: '1.5rem',
-              maxWidth: '1.5rem',
-            }}
-          />
-        </figure>
-      </ul>
-    ) : type === 'FOLD' ? (
-      <ul className="flex h-full flex-col items-center lg:flex-row">
-        {menuItems.map((item, index) => (
-          <MenuItem
-            type={type}
-            key={index}
-            isActive={pathname.includes(item.targetPath)}
-            {...item}
-          />
-        ))}
-      </ul>
-    ) : null
-  ) : (
-    <ul className="flex h-full flex-row items-center pt-1 lg:flex-row">
-      {menuItems.map((item, index) => (
-        <MenuItem
-          type={type}
+  return (
+    <ul className="flex h-full w-full flex-row items-center pt-1 lg:flex-row">
+      {MENU_ITEMS.map((menuItem, index) => (
+        <MenuButton
           key={index}
-          isActive={pathname.includes(item.targetPath)}
-          {...item}
+          menuItem={menuItem}
+          isActive={pathname === menuItem.targetPath}
         />
       ))}
     </ul>
   );
+  // return windowWidth < 880 ? (
+  //   type === 'UNFOLD' ? (
+  //     <ul className="flex h-full w-full flex-row items-center justify-end pt-1 lg:flex-row">
+  //       <figure
+  //         onClick={() => onClickMenu((menuFlag) => !menuFlag)}
+  //         className="cursor-pointer"
+  //       >
+  //         <Image
+  //           src="/assets/icons/menu.svg"
+  //           alt="menu.svg"
+  //           priority={true}
+  //           width={36}
+  //           height={32}
+  //           style={{
+  //             minWidth: '1.5rem',
+  //             maxWidth: '1.5rem',
+  //           }}
+  //         />
+  //       </figure>
+  //     </ul>
+  //   ) : type === 'FOLD' ? (
+  //     <ul className="flex h-full flex-col items-center lg:flex-row">
+  //       {menuItems.map((item, index) => (
+  //         <MenuItem
+  //           type={type}
+  //           key={index}
+  //           isActive={pathname === item.targetPath}
+  //           {...item}
+  //         />
+  //       ))}
+  //     </ul>
+  //   ) : null
+  // ) : (
+
+  // );
 }
--- core/web-app/src/app/app/layout.tsx
+++ core/web-app/src/app/app/layout.tsx
@@ -10,95 +10,86 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const pathname = usePathname();
 
-  const [windowWidth, setWindowWidth] = useState<number>(0);
+  // const [windowWidth, setWindowWidth] = useState<number>(0);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setWindowWidth(window.innerWidth);
+  //   };
 
-    setWindowWidth(window.innerWidth);
-    window.addEventListener('resize', handleResize);
+  //   setWindowWidth(window.innerWidth);
+  //   window.addEventListener('resize', handleResize);
 
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize);
+  //   };
+  // }, []);
 
-  const [menuFlag, setMenuFlag] = useState<boolean>(false);
+  // const [menuFlag, handleClickMenu] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (windowWidth >= 880) {
-      setMenuFlag(false);
-    }
-  }, [windowWidth]);
+  // useEffect(() => {
+  //   if (windowWidth >= 880) {
+  //     handleClickMenu(false);
+  //   }
+  // }, [windowWidth]);
 
-  useEffect(() => {
-    setMenuFlag(false);
-  }, [pathname]);
+  // useEffect(() => {
+  //   handleClickMenu(false);
+  // }, [pathname]);
 
-  const menuRef = useRef<HTMLInputElement>(null);
+  // const menuRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    const clickOutside = (event: any) => {
-      if (
-        menuFlag &&
-        menuRef.current &&
-        !menuRef.current?.contains(event.target)
-      ) {
-        setMenuFlag(false);
-      }
-    };
-    document.addEventListener('mousedown', clickOutside);
-    return () => {
-      document.removeEventListener('mousedown', clickOutside);
-    };
-  }, [menuFlag]);
+  // useEffect(() => {
+  //   const clickOutside = (event: any) => {
+  //     if (
+  //       menuFlag &&
+  //       menuRef.current &&
+  //       !menuRef.current?.contains(event.target)
+  //     ) {
+  //       handleClickMenu(false);
+  //     }
+  //   };
+  //   document.addEventListener('mousedown', clickOutside);
+  //   return () => {
+  //     document.removeEventListener('mousedown', clickOutside);
+  //   };
+  // }, [menuFlag]);
 
   return (
     <div className="h-screen w-screen min-w-screen-md overflow-auto">
       <nav
-        className="navbar relative h-14 w-full p-0 text-neutral-content"
-        ref={menuRef}
+        className="navbar relative w-full px-10 py-0 text-neutral-content h-0 min-h-16"
+        // ref={menuRef}
       >
-        <div className="flex h-full w-full items-center px-8">
-          <div className="flex h-full w-full items-center justify-between">
-            <figure
-              onClick={() => router.push('/app')}
-              className="cursor-pointer"
-            >
-              <Image
-                src="/assets/images/wave-autoscale_symbol.svg"
-                alt="wave-autoscale_symbol.svg"
-                priority={true}
-                width={36}
-                height={32}
-                style={{
-                  minWidth: '2.5rem',
-                  maxWidth: '2.5rem',
-                }}
-              />
-            </figure>
-            <div className="ml-10 flex h-full w-full items-center">
-              <Menu
-                type="UNFOLD"
-                windowWidth={windowWidth}
-                menuFlag={menuFlag}
-                setMenuFlag={setMenuFlag}
-              />
-            </div>
-          </div>
-        </div>
-        {menuFlag && (
+        {/* Logo */}
+        <figure
+          onClick={() => router.push('/app')}
+          className="mr-10 cursor-pointer"
+        >
+          <Image
+            src="/assets/images/wave-autoscale_symbol.svg"
+            alt="wave-autoscale_symbol.svg"
+            priority={true}
+            width={36}
+            height={32}
+            style={{
+              minWidth: '2.5rem',
+              maxWidth: '2.5rem',
+            }}
+          />
+        </figure>
+        {/* Menu */}
+        <Menu />
+        {/* {menuFlag && (
           <div className="absolute top-16 z-30 flex w-full flex-col items-center overflow-y-auto border-b border-gray-200 bg-base-100 px-8 py-8">
             <Menu
               type="FOLD"
               windowWidth={windowWidth}
               menuFlag={menuFlag}
-              setMenuFlag={setMenuFlag}
+              onClickMenu={setMenuFlag}
             />
           </div>
-        )}
+        )} */}
       </nav>
       <main className="wa-main">{children}</main>
     </div>
