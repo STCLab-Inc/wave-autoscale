2da3f6ce575810cf914dbdb662c47ed9045fcdb5
Hwansoo Kim <pueding@gmail.com>
2024-01-18 18:16:17 +0900

    fix(web-app): parse the timestamp with a variety of formats

---
+++ core/web-app/src/utils/date.ts
@@ -0,0 +1,24 @@
+import dayjs, { Dayjs } from 'dayjs';
+
+// Regex to match unix timestamp
+const unixTimestampRegex = /^\d{10}$/;
+const unixTimestampMsRegex = /^\d{13}$/;
+const unixTimestampMsWithPointRegex = /^\d{10}\.\d{3}$/;
+
+export function parseDateToDayjs(date: Date | string | number): Dayjs {
+  // If date is Date type
+  if (date instanceof Date) {
+    return dayjs(date);
+  }
+  // If date is string or number type
+  // If date is unix timestamp
+  if (
+    unixTimestampRegex.test(date.toString()) ||
+    unixTimestampMsRegex.test(date.toString()) ||
+    unixTimestampMsWithPointRegex.test(date.toString())
+  ) {
+    return dayjs.unix(Number(date));
+  }
+  // Else, parse date with dayjs
+  return dayjs(date);
+}
--- core/web-app/src/utils/router.ts
+++
@@ -1,6 +0,0 @@
-import { ReadonlyHeaders } from 'next/dist/server/web/spec-extension/adapters/headers';
-
-export function getPathname(headers: ReadonlyHeaders) {
-  console.dir(headers);
-  return headers.get('next-url');
-}
