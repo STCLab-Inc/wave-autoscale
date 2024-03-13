82fe364c14ee178de4b0542142138882a123fd82
Hwansoo Kim <pueding@gmail.com>
2024-03-14 00:33:35 +0900

    chore(web-app): remove unused code

--- core/web-app/src/app/app/plan-logs/status-badge.tsx
+++
@@ -1,6 +0,0 @@
-export default function StatusBadge({ success }: { success: boolean }) {
-  if (success) {
-    return <div className="badge-success badge p-2 text-xs">Success</div>;
-  }
-  return <div className="badge-error badge p-2 text-xs">Fail</div>;
-}
